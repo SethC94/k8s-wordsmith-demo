7ccfb86d015c94aa65ca9e00bf2744ca17f8b0b6
Seth Creasman <sethcreasman@geminidata.com>
2020-10-15 19:24:18 -0700

    file edit: modifying unit test file dir for inside container

--- unit_test.sh
+++ unit_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Checking formatting..."
-if [ -n "$(gofmt -s -l /Users/sethcreasman/src/codefresh/k8s-wordsmith-demo/web/dispatcher.go)" ]; then
+if [ -n "$(gofmt -s -l ./web/dispatcher.go)" ]; then
     echo "Code is not formatted. Run 'gofmt -w .'"
     exit 1
 fi
